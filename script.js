e123a401c296d87c872fcf3954913d91986dab95
Vivek kumar <vivek8969261413@gmail.com>
2026-03-02 15:25:47 +0530

    changed fetched link of song

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function secondsToMinutesSeconds(seconds) {
 let currentSong = new Audio();
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let a = await fetch("https://spotify-clone-lac-psi.vercel.app/songs/")
     let response = await a.text();
     console.log(response)
     let div = document.createElement("div")
